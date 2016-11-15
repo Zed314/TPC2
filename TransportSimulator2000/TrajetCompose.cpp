b884b69899489345bf30568aab4f2db85b3c2a4e
ziggy.vergne <ziggy.vergne.insa-lyon.fr>
2016-11-15 16:38:16 +0100

    Dossiers Ajoutés

---
+++ TransportSimulator2000/TrajetCompose.cpp
@@ -0,0 +1,77 @@
+
+/*************************************************************************
+                           TrajetCompose  -  classe représentant un TrajetCompose 
+                             -------------------
+    début                : 15/11/2016
+    copyright            : (C) 2016 par Lucas POISSE & Ziggy VERGNE
+    e-mail               : lucas.poisse@insa-lyon.fr / ziggy.vergne@insa-lyon.fr
+     
+*************************************************************************/
+
+//---------- Réalisation de la classe <TrajetCompose> (fichier TrajetCompose.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "TrajetCompose.h"
+#include "ListeTrajets.h"
+#include "Trajet.h"
+
+//------------------------------------------------------------- Constantes
+
+//---------------------------------------------------- Variables de classe
+
+//----------------------------------------------------------- Types privés
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- Méthodes publiques
+// type TrajetCompose::Méthode ( liste de paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+//-------------------------------------------- Constructeurs - destructeur
+TrajetCompose::TrajetCompose ( const TrajetCompose & unTrajetCompose )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <TrajetCompose>" << endl;
+#endif
+} //----- Fin de TrajetCompose::TrajetCompose (constructeur de copie)
+
+
+TrajetCompose::TrajetCompose ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <TrajetCompose>" << endl;
+#endif
+} //----- Fin de TrajetCompose
+
+
+TrajetCompose::~TrajetCompose ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <TrajetCompose>" << endl;
+#endif
+} //----- Fin de ~TrajetCompose
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
+//------------------------------------------------------- Méthodes privées
+
---
+++ TransportSimulator2000/TrajetSimple.cpp
@@ -0,0 +1,75 @@
+
+/*************************************************************************
+                           TrajetSimple  -  classe représentant un TrajetSimple
+                             -------------------
+    début                : 15/11/2016
+    copyright            : (C) 2016 par Lucas POISSE & Ziggy VERGNE
+    e-mail               : lucas.poisse@insa-lyon.fr / ziggy.vergne@insa-lyon.fr
+     
+*************************************************************************/
+
+//---------- Réalisation de la classe <TrajetSimple> (fichier TrajetSimple.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "TrajetSimple.h"
+#include "Trajet.h"
+//------------------------------------------------------------- Constantes
+
+//---------------------------------------------------- Variables de classe
+
+//----------------------------------------------------------- Types privés
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- Méthodes publiques
+// type TrajetSimple::Méthode ( liste de paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+//-------------------------------------------- Constructeurs - destructeur
+TrajetSimple::TrajetSimple ( const TrajetSimple & unTrajet )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <TrajetSimple>" << endl;
+#endif
+} //----- Fin de TrajetSimple::TrajetSimple (constructeur de copie)
+
+
+TrajetSimple::TrajetSimple ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <TrajetSimple>" << endl;
+#endif
+} //----- Fin de TrajetSimple
+
+
+TrajetSimple::~TrajetSimple ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <TrajetSimple>" << endl;
+#endif
+} //----- Fin de ~TrajetSimple
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
+//------------------------------------------------------- Méthodes privées
+
